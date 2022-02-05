daf7ba06ef7ccfd169b59446dbffb05cb42e8077
sriramsaranp <psriramsaran@gmail.com>
2022-02-05 15:57:35 +0530

    quiz game code added

---
+++ Day_17/main.py
@@ -0,0 +1,15 @@
+from data import question_data
+from question_model import Question
+from quiz_brain import QuizBrain
+
+question_bank = []
+
+for item in question_data:
+    question_bank.append(Question(item['text'], item['answer']))
+
+
+quiz_brain = QuizBrain(question_bank)
+
+while quiz_brain.still_has_questions():
+    quiz_brain.next_question()
+
---
+++ Day_17/quiz_brain.py
@@ -0,0 +1,28 @@
+#Asking the question
+#Checking if the answer is correct
+#Check if we have reached the end
+
+
+class QuizBrain:
+    def __init__(self, q_list):
+        self.question_number = 0
+        self.question_list = q_list
+        self.score = 0
+
+    def next_question(self):
+        user_answer = input(f"Q.{self.question_number+1}: {self.question_list[self.question_number].text} (True/False): ")
+        current_question = self.question_list[self.question_number]
+        self.check_answer(user_answer, current_question.answer)
+        self.question_number += 1
+
+    def still_has_questions(self):
+        return self.question_number < len(self.question_list)
+
+    def check_answer(self, user_answer, correct_answer):
+        if user_answer.lower() == correct_answer.lower() :
+            print("You got Correct!")
+            self.score += 1
+        else:
+            print("You got Wrong!")
+        print(f"The correct answer is {correct_answer}")
+        print(f"Your score is {self.score}/{self.question_number+1}")
