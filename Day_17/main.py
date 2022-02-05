680420e05ec5bc6e2301ae29e437dd82c7ea1a99
sriramsaranp <psriramsaran@gmail.com>
2022-02-05 16:04:45 +0530

    added final print statements

--- Day_17/main.py
+++ Day_17/main.py
@@ -13,3 +13,6 @@ quiz_brain = QuizBrain(question_bank)
 while quiz_brain.still_has_questions():
     quiz_brain.next_question()
 
+
+print("You have finished the quiz!")
+print(f"Your score is {quiz_brain.score}/{len(question_bank)}")
