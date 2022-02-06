3557771e08cbe7d0b46bf42ba848b8e4fde7692c
sriramsaranp <psriramsaran@gmail.com>
2022-02-06 13:05:52 +0530

    added random walk code

--- Day_18/main.py
+++ Day_18/main.py
@@ -1,20 +1,35 @@
 from turtle import Turtle, Screen 
 import random
+import turtle
 
 timmy = Turtle()
-timmy.shape("turtle")
+#timmy.shape("turtle")
 timmy.color("blue")
 
-colors = [ "red", "green", "blue", "orange", "purple", "yellow", "pink", "cyan", "magenta" ]
+turtle.colormode(255)
 
-for i in range(3,11):
-    for j in range (i):
-        timmy.color(colors[random.randint(0,8)])
-        timmy.forward(75)
-        timmy.right(360/i)
+def get_color():
+    r = random.randint(0,255)
+    g = random.randint(0,255)
+    b = random.randint(0,255)
+    return (r,g,b) 
 
 
+dirs = [0,90,180,270]
+
+def movement(dir):
+    timmy.speed("fastest")
+    timmy.pensize(5)
+    timmy.color(get_color())
+    timmy.forward(30)
+    timmy.setheading(dir)
 
 screen = Screen()
+for _ in range(100):
+    direction = random.choice(dirs)
+    movement(direction)
+
 screen.exitonclick()
+
+
 
